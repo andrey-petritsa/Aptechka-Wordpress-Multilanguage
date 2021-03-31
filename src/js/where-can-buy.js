3d36cd81e72dc54e4c857e3a9dccc0ad044203b0
Andrey Petritsa <andrey@petritsa.ru>
2021-03-31 18:02:14 +0300

    Поменял нейминг блока где купить по бэму

--- src/js/where-can-buy.js
+++ src/js/where-can-buy.js
@@ -1,22 +1,22 @@
 export default function initWhereCanBuy() {
-    const links = document.querySelectorAll(`.where-can-buy-menu__link`)
+    const links = document.querySelectorAll(`.fade-out-tab-menu__link`)
     if (links) {
         links.forEach((link) => {
             link.addEventListener(`click`, function () {
                 // eslint-disable-next-line no-shadow
                 links.forEach((link) => {
-                    link.classList.remove(`where-can-buy-menu__link_clicked`)
+                    link.classList.remove(`fade-out-tab-menu__link_clicked`)
                 })
-                this.classList.toggle(`where-can-buy-menu__link_clicked`)
+                this.classList.toggle(`fade-out-tab-menu__link_clicked`)
                 const currentProductToShow = this.dataset.product
 
-                const products = document.querySelectorAll(`.where-can-buy__product`)
+                const products = document.querySelectorAll(`.fade-out-tab__item`)
                 products.forEach((product) => {
-                    product.classList.remove(`where-can-buy__product_active`)
+                    product.classList.remove(`fade-out-tab__item_active`)
                 })
                 products.forEach((product) => {
                     if (product.dataset.product === currentProductToShow) {
-                        product.classList.toggle(`where-can-buy__product_active`)
+                        product.classList.toggle(`fade-out-tab__item_active`)
                     }
                 })
             })
