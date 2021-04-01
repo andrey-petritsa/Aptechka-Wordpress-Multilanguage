6c1b3e5ab3c8d945d4ea2cecdd378d025e5beb89
Andrey Petritsa <andrey@petritsa.ru>
2021-04-01 12:59:45 +0300

    Переделал блок где купить, сделал рефакторинг кода блока где купить

--- src/js/where-can-buy.js
+++ src/js/where-can-buy.js
@@ -2,21 +2,22 @@ export default function initWhereCanBuy() {
     const links = document.querySelectorAll(`.fade-out-tab-menu__link`)
     if (links) {
         links.forEach((link) => {
-            link.addEventListener(`click`, function () {
+            link.addEventListener(`click`, function (e) {
+            	e.preventDefault()
                 // eslint-disable-next-line no-shadow
                 links.forEach((link) => {
                     link.classList.remove(`fade-out-tab-menu__link_clicked`)
                 })
-                this.classList.toggle(`fade-out-tab-menu__link_clicked`)
-                const currentProductToShow = this.dataset.product
+                e.target.classList.toggle(`fade-out-tab-menu__link_clicked`)
+                const currentProductToShow = e.target.dataset.product
 
-                const products = document.querySelectorAll(`.fade-out-tab__item`)
+                const products = document.querySelectorAll(`.fade-out-tab__section`)
                 products.forEach((product) => {
-                    product.classList.remove(`fade-out-tab__item_active`)
+                    product.classList.remove(`fade-out-tab__section_active`)
                 })
                 products.forEach((product) => {
                     if (product.dataset.product === currentProductToShow) {
-                        product.classList.toggle(`fade-out-tab__item_active`)
+                        product.classList.toggle(`fade-out-tab__section_active`)
                     }
                 })
             })
